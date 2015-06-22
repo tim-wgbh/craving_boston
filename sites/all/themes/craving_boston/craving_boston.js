850d0ae00023cdbdbeeff93c2179ee5a0495c646
Tim Kinnel <tim_kinnel@wgbh.org>
2015-06-22 10:41:16 -0400

    Fix appearance of photo credits

--- sites/all/themes/craving_boston/craving_boston.js
+++ sites/all/themes/craving_boston/craving_boston.js
@@ -2,14 +2,18 @@
 
 Drupal.behaviors.cravingBostonTheme = {
   attach: function (context) {
+      
     // Create captions for images with titles
     $('.field-name-field-image picture, .field-name-body img').each(function() {
       var $element = $(this);
-      var title = $element.attr('title').trim();
-      if (title != '') {
+      var title = $element.attr('title');
+      if (typeof title !== typeof undefined && title !== false && title.trim() != '') {
         $element.wrap('<div class="image-with-caption"></div>');
         $element.parent().append('<div class="image-caption">Photo: ' + title + '</div>') 
       }
+    });
+    $('.image-with-caption').each(function() {
+      if ($(this).parent().prop('tagName') == 'STRONG') $(this).unwrap().unwrap();
     });
   }
 };
