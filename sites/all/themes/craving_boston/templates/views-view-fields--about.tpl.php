8d74dffafefce6bc49e81bae2108465b3ad7704b
Tim Kinnel <tim_kinnel@wgbh.org>
2015-09-14 15:37:07 -0400

    Styling changes to faqs and about

---
+++ sites/all/themes/craving_boston/templates/views-view-fields--about.tpl.php
@@ -0,0 +1,7 @@
+<div class="row">
+  <div class="col-md-4 col-sm-12"><?php print $fields['field_image']->content; ?></div>
+  <div class="col-md-8 col-sm-12">
+    <div class="name"><?php print $fields['title']->content; ?></div>
+    <div class="bio-text"><?php print $fields['body']->content; ?></div>
+  </div>
+</div>
---
+++ sites/all/themes/craving_boston/templates/views-view-fields--faqs.tpl.php
@@ -0,0 +1,4 @@
+<div class="well well-sm">
+  <div class="question"><?php print $fields['title']->content; ?></div>
+  <div class="answer"><?php print $fields['body']->content; ?></div>
+</div>
