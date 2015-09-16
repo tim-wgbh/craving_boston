dc6547dd5d09d59968d1c642e68133976a035e95
Tim Kinnel <tim_kinnel@wgbh.org>
2015-09-16 15:05:19 -0400

    Redo top WGBH links with table, add newsleter signup block code from Nancy

--- sites/all/modules/wgbh_branding/js/wgbh_branding.js
+++ sites/all/modules/wgbh_branding/js/wgbh_branding.js
@@ -6,11 +6,11 @@ Drupal.behaviors.wgbhBrandingModule = {
     $('body').css({'margin-top': '0'});
     // Create WGBH links
     var links =
-      "<div class='wgbh-links'>\n" +
-      "  <div class='wgbh-logo'><a href='http://www.wgbh.org' title='WGBH.org'  class='image'><img src='/sites/all/modules/wgbh_branding/images/wgbh_mini_logo.png' alt='WGBH' /></a></div>\n" +
-      "  <div class='my-wgbh'><a href='http://www.wgbh.org/mywgbh' title='Go to myWGBH'>myWGBH</a></div>\n" +
-      "  <div class='donate-button'><a href='http://www.wgbh.org/donateButton' title='Donate to WGBH'>Donate</a></div>\n" +
-      "</div>\n";      
+      "<table class='wgbh-links'><tr>\n" +
+      "  <td class='wgbh-logo'><a href='http://www.wgbh.org' title='WGBH.org'  class='image'><img src='/sites/all/modules/wgbh_branding/images/wgbh_mini_logo.png' alt='WGBH' /></a></td>\n" +
+      "  <td class='actions'><span class='my-wgbh'><a href='http://www.wgbh.org/mywgbh' title='Go to myWGBH'>myWGBH</a></span> \n" +
+      "    <span class='donate-button'><a href='http://www.wgbh.org/donateButton' title='Donate to WGBH'>Donate</a></span></td>\n" +
+      "</table>\n";      
     
     
     $('body').prepend(links);
