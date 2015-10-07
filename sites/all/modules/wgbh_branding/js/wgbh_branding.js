1d099f538b266e8f24c7fcff7db8fe9472d0c744
Tim Kinnel <tim_kinnel@wgbh.org>
2015-10-07 10:21:48 -0400

    Fix image styles in feature, change donate link

--- sites/all/modules/wgbh_branding/js/wgbh_branding.js
+++ sites/all/modules/wgbh_branding/js/wgbh_branding.js
@@ -9,7 +9,7 @@ Drupal.behaviors.wgbhBrandingModule = {
       "<table class='wgbh-links'><tr>\n" +
       "  <td class='wgbh-logo'><a href='http://www.wgbh.org' title='WGBH.org'  class='image'><img src='/sites/all/modules/wgbh_branding/images/wgbh_mini_logo.png' alt='WGBH' /></a></td>\n" +
       "  <td class='actions'><span class='my-wgbh'><a href='http://www.wgbh.org/mywgbh' title='Go to myWGBH'>myWGBH</a></span> \n" +
-      "    <span class='donate-button'><a href='http://www.wgbh.org/donateButton' title='Donate to WGBH'>Donate</a></span></td>\n" +
+      "    <span class='donate-button'><a href='http://www.wgbh.org/support/cravingbostonweb_makeagift.cfm' title='Donate to WGBH'>Donate</a></span></td>\n" +
       "</table>\n";      
     
     
