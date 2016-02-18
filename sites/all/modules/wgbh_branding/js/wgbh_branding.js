97a495bc81a0439bd5825eb75af6c9eccecb52aa
Tim Kinnel <tim_kinnel@wgbh.org>
2016-02-18 09:59:26 -0500

    Add ember theme, navbar module  and supporting libraries for new admin theme

--- sites/all/modules/wgbh_branding/js/wgbh_branding.js
+++ sites/all/modules/wgbh_branding/js/wgbh_branding.js
@@ -3,19 +3,23 @@
 Drupal.behaviors.wgbhBrandingModule = {
   attach: function (context) {
     
-    $('body').css({'margin-top': '0'});
-    // Create WGBH links
-    var links =
-      "<table class='wgbh-links'><tr>\n" +
-      "  <td class='wgbh-logo'><a href='http://www.wgbh.org' title='WGBH.org'  class='image'><img src='/sites/all/modules/wgbh_branding/images/wgbh_mini_logo.png' alt='WGBH' /></a></td>\n" +
-      "  <td class='actions'><span class='my-wgbh'><a href='http://www.wgbh.org/mywgbh' title='Go to myWGBH'>myWGBH</a></span> \n" +
-      "    <span class='donate-button'><a href='http://www.wgbh.org/support/cravingbostonweb_makeagift.cfm' title='Donate to WGBH'>Donate</a></span></td>\n" +
-      "</table>\n";      
+    if (!(window.location.pathname.match(/^\/admin/) ||
+          window.location.pathname.match(/^\/node\/add/) ||
+          window.location.pathane.match(/^\/node\/\d+\/edit/)) {
+      $('body').css({'margin-top': '0'});
+      // Create WGBH links
+      var links =
+        "<table class='wgbh-links'><tr>\n" +
+        "  <td class='wgbh-logo'><a href='http://www.wgbh.org' title='WGBH.org'  class='image'><img src='/sites/all/modules/wgbh_branding/images/wgbh_mini_logo.png' alt='WGBH' /></a></td>\n" +
+        "  <td class='actions'><span class='my-wgbh'><a href='http://www.wgbh.org/mywgbh' title='Go to myWGBH'>myWGBH</a></span> \n" +
+        "    <span class='donate-button'><a href='http://www.wgbh.org/support/cravingbostonweb_makeagift.cfm' title='Donate to WGBH'>Donate</a></span></td>\n" +
+        "</table>\n";      
     
     
-    $('body').prepend(links);
-    $('body').animate({ 'margin-top' : '47px' });
-    $('body .wgbh-links').animate({ 'top' : '0' });
+      $('body').prepend(links);
+      $('body').animate({ 'margin-top' : '47px' });
+      $('body .wgbh-links').animate({ 'top' : '0' });
+    }
   }
 };
 
