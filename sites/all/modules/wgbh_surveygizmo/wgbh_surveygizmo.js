368622f3eb0644a6639fe4f23e0179ef229554b9
Tim Kinnel <tim_kinnel@wgbh.org>
2016-02-24 10:15:42 -0500

    Display survey popup in modal

--- sites/all/modules/wgbh_surveygizmo/wgbh_surveygizmo.js
+++ sites/all/modules/wgbh_surveygizmo/wgbh_surveygizmo.js
@@ -2,7 +2,7 @@
 
 Drupal.behaviors.wgbhSurveyGizmoModule = {
   attach: function (context) {
-    $('#sg-popup').wrap('<div id="sg-popup-wrapper"></div>');
+    $('#sg-popup').wrap('<div id="sg-modal"><div id="sg-popup-wrapper"></div></div>');
     $('#sg-popup').show();
   }
 };
