94cf55d83e9f44603e127494423edd1e7c40c8fe
Tim Kinnel <tim_kinnel@wgbh.org>
2016-03-10 10:09:55 -0500

    Template and style changes for related content - 3-up

--- sites/all/themes/craving_boston/templates/views-view-fields--related-content.tpl.php
+++ sites/all/themes/craving_boston/templates/views-view-fields--related-content.tpl.php
@@ -1,10 +1,4 @@
 <div class="related-row">
-  <div class="related-image">
-    <?php print $fields['field_image']->content; ?>
-  </div>
-  <div class="text">
-    <h3><?php print $fields['title']->content; ?></h3>
-    <p><?php print $fields['field_subhead']->content; ?></p>
-  </div>
-  <div class="clearfix"></div>
+  <?php print $fields['field_image']->content; ?>
+  <h3><?php print $fields['title']->content; ?></h3>
 </div>
