e870b941ed914aacd71b4fcd6b04111d9e148752
Tim Kinnel <tim_kinnel@wgbh.org>
2016-04-05 15:31:12 -0400

    Updates for what's popular to get icons

---
+++ sites/all/themes/craving_boston/templates/views-view-fields--most-popular-now.tpl.php
@@ -0,0 +1,10 @@
+<div class="views-row">
+  <?php if ($view->current_display == 'block'): ?>
+    <div class="views-field-field-image">
+      <?php print $image; ?>
+    </div>
+  <?php endif; ?>
+  <div class="views-field-title">
+    <?php print $headline; ?>
+  </div>
+</div>
