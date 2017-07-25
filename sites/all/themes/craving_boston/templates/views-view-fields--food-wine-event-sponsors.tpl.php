9cca471afd63dbd1d32a39888fdd3b2ee64ff6eb
Tim Kinnel <tim_kinnel@wgbh.org>
2017-07-25 15:17:01 -0400

    Fix link popup behind image links for food and wine

--- sites/all/themes/craving_boston/templates/views-view-fields--food-wine-event-sponsors.tpl.php
+++ sites/all/themes/craving_boston/templates/views-view-fields--food-wine-event-sponsors.tpl.php
@@ -2,7 +2,6 @@
   Overrides the display settings in the UI
   We want a logo image linked to the URL
 -->
-<?php dpm($fields); ?>
-<a href="<?php print $fields['field_link']->content; ?>" target="_blank">
+<a class="image" href="<?php print $fields['field_link']->content; ?>" target="_blank">
   <?php print $fields['field_image']->content; ?>
 </a>
